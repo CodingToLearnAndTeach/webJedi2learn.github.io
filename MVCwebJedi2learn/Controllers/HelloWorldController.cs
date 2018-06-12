5a9619dc05f8252ff7cdce7f42038bd73e92efbf
CodeOrNotToCode <CodeOrNotToCode@outlook.com>
2018-06-12 12:14:21 -0500

    Extending the controller functions.
    Practicing with processing different links sent to the Controller class.

---
+++ MVCwebJedi2learn/Controllers/HelloWorldController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVCwebJedi2learn.Controllers
+{
+    /*
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+    */
+
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        /*
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+        */
+
+            /*
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+        */
+
+        public string Welcome(string name, int ID = 100)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+    }
+}
